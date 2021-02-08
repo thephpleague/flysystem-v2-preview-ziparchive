a1493571c4290f76d7fed1bbd9e2c542327fab90
Frank de Jonge <info@frankdejonge.nl>
2021-02-08 21:49:53 +0100

    Imported version 2.0.1

---
+++ ZipArchiveException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace League\FlysystemV2Preview\ZipArchive;
+
+use League\FlysystemV2Preview\FilesystemException;
+
+interface ZipArchiveException extends FilesystemException
+{
+}
